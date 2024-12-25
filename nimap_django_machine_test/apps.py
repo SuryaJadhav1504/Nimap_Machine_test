a8f62f3f5164b245d5de36db57997c8b65bfa187
SuryaJadhav1504 <jadhavsurya12345@gmail.com>
2024-12-25 14:14:58 +0530

    Initial commit

---
+++ nimap_django_machine_test/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NimapDjangoMachineTestConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nimap_django_machine_test'
