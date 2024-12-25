50b32ee5bb047809780ad91d46d6b3873854240b
SuryaJadhav1504 <jadhavsurya12345@gmail.com>
2024-12-25 16:34:27 +0530

    Surya 25-12-2024

--- nimap_project/settings.py
+++ nimap_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nimap_django_machine_test',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +75,22 @@ WSGI_APPLICATION = 'nimap_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Machine_test_django',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
